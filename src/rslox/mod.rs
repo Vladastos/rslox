f4954be7d852fe3e0c266aba36749165001e72be
Vladislav Parfeniuc <v.parfeniuc@hastega.it>
2025-02-25 19:10:27 +0100

    wip

--- src/rslox/mod.rs
+++ src/rslox/mod.rs
@@ -99,7 +99,7 @@ pub enum LoxError {
         ScannerError,
     ),
     // this is an horrible way to solve the problem of multiple sources
-    #[error("Syntax error: {}", _0.iter().map(|e| format!("\t{e} \n")).collect::<String>())]
+    #[error("Syntax error: {} \n", _0.iter().map(|e| format!("\t{e} \n")).collect::<String>())]
     ParsingError(Vec<ParserError>),
     #[error("Runtime error: {0}")]
     RuntimeError(
